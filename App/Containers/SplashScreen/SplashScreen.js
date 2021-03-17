e3dbcc2fad6a9c9804278c942678146728a817e8
alirezatk <alirezatk73@gmail.com>
2021-03-17 12:58:36 +0300

    added splash screen

--- App/Containers/SplashScreen/SplashScreen.js
+++ App/Containers/SplashScreen/SplashScreen.js
@@ -4,18 +4,16 @@ import styles from "./SplashScreenStyle";
 import { Helpers } from "App/Theme";
 import { useNavigation } from "@react-navigation/core";
 import Strings from "../../Values/Strings";
+import RNBootSplash from "react-native-bootsplash";
 
 const SplashScreen = () => {
   const { navigate } = useNavigation();
   useEffect(() => {
     navigate(Strings.Routes.HOME);
+    setTimeout(() => {
+      RNBootSplash.hide({ fade: true });
+    }, 2000);
   }, []);
-  return (
-    <View style={[Helpers.fillRowCenter, styles.container]}>
-      <View style={[Helpers.center, styles.logo]}>
-        <Text>LOGO</Text>
-      </View>
-    </View>
-  );
+  return null;
 };
 export default SplashScreen;
